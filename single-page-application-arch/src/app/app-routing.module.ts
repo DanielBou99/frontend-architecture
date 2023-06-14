9f545f341af8bc7ad18d0dfce78550e0628a6e8e
Daniel Bou de Souza <daniel.bou@nava.com.br>
2023-06-14 13:58:43 -0300

    Components, bootstrap and routing

--- single-page-application-arch/src/app/app-routing.module.ts
+++ single-page-application-arch/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AnimalsComponent } from './pages/animals/animals.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: WelcomeComponent
+  },
+  {
+    path: 'animals',
+    component: AnimalsComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- single-page-application-arch/src/app/app.module.ts
+++ single-page-application-arch/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AnimalsComponent } from './pages/animals/animals.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    WelcomeComponent,
+    ContactComponent,
+    AnimalsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
